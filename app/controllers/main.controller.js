733efaa8aab1a28868831ce3b071cfbe9b94cfed
Frank Samuel <frankdotwales@gmail.com>
2017-06-25 11:51:56 +0100

    Added template engine and basic layout/view

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -5,6 +5,8 @@ module.exports = {
 
   // Show the homepage
   showHome: (req, res) => {
+    // Render method to look at templates
+    res.render('pages/home');
     res.send('Hello world!!!');
   }
 
--- server.js
+++ server.js
@@ -2,8 +2,15 @@
 const express = require('express'),
   app = express(),
   port = process.env.PORT || 8080;
+  expressLayouts = require('express-ejs-layouts');
 
 // Configure the application
+// Tell express where to look for static assets
+app.use(express.static(__dirname + '/public'));
+
+// Set ejs as our templating engine
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 // Set the routes
 app.use(require('./app/routes'));
