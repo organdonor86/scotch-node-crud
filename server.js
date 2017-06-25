9c97a733bb5322085a3ee100c41150860c37f253
Frank Samuel <frankdotwales@gmail.com>
2017-06-25 13:06:24 +0100

    Added linkl to mLab database, setup enviornment variables file

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+// Load enviornment variables
+require('dotenv').config();
+
 // Grab dependencies
 const express = require('express'),
   app = express(),
   port = process.env.PORT || 8080;
   expressLayouts = require('express-ejs-layouts');
+  mongoose = require('mongoose');
 
 // Configure the application
 // Tell express where to look for static assets
@@ -11,6 +15,9 @@ app.use(express.static(__dirname + '/public'));
 // Set ejs as our templating engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+// Connect to database using database URI
+mongoose.connect(process.env.DB_URI);
 
 // Set the routes
 app.use(require('./app/routes'));
